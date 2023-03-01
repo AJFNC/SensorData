1fa760f1c61fb47cec3646c97eec673c89e3d118
AJFNC <ajfnc@hotmail.com>
2023-02-28 22:13:20 -0300

    Adaptação do dashboard para uso na Bluewater

--- SensorData/wwwroot/lib/bootstrap/dist/js/dashboard.js
+++ SensorData/wwwroot/lib/bootstrap/dist/js/dashboard.js
@@ -6,35 +6,35 @@
   feather.replace({ 'aria-hidden': 'true' })
 
   // Graphs
-  const ctx = document.getElementById('myChart')
+  const ctx = document.getElementById('lineChart')
   // eslint-disable-next-line no-unused-vars
   const myChart = new Chart(ctx, {
     type: 'line',
     data: {
       labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
+        '10:00',
+        '10:30',
+        '11:00',
+        '11:30',
+        '12:00',
+        '12:30',
+        '13:00'
       ],
       datasets: [{
         data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
+         0.893,
+         0.894,
+         0.899,
+         0.896,
+         0.905,
+         0.901,
+         0.918
         ],
         lineTension: 0,
         backgroundColor: 'transparent',
         borderColor: '#007bff',
         borderWidth: 4,
-        pointBackgroundColor: '#007bff'
+        pointBackgroundColor: '#008bff'
       }]
     },
     options: {
