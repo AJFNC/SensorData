e234b64598ba7419c8eb29fa2a4246422369d30c
AJFNC <ajfnc@hotmail.com>
2023-04-25 22:38:14 -0300

    Criado View page e Controller método para tabela Bananas similar a SS

--- SensorData/Models/Frequency.cs
+++ SensorData/Models/Frequency.cs
@@ -1,12 +1,23 @@
-﻿namespace SensorData.Models
+﻿using System.ComponentModel;
+
+namespace SensorData.Models
 {
     public class Frequency
     {
         public int Id { get; set; }
+
+        [DisplayName("Sensor")]
         public int Sensor_Id { get; set; }
+
+        [DisplayName("SS: 0 - 20")]
         public float Frl1 { get; set;}
+
+        [DisplayName("SS: 20 - 40")]
         public float Frl2 { get; set;}
+        [DisplayName("SS: 40 - 60")]
         public float Frl3 { get; set;}
+
+        [DisplayName("Hora")]
         public DateTime ReadDateTime { get; set;} = DateTime.Now.ToLocalTime();
     }
 }
